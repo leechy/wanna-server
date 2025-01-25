99af11067924a3e6b2aa82f39c0769ec077ec906
Andrey Lechev <andrey@lechev.me>
2025-01-25 10:48:08 +0100

    Add two more tables to the DB — items and list_items

--- database-structure.sql
+++ database-structure.sql
@@ -106,3 +106,69 @@ ALTER TABLE IF EXISTS public.user_lists
 
 COMMENT ON TABLE public.user_lists
     IS 'Link between the users and the lists';
+
+
+DROP TABLE IF EXISTS public.items
+
+CREATE TABLE public.items
+(
+    item_id uuid NOT NULL DEFAULT gen_random_uuid(),
+    name text,
+    units text,
+    type text NOT NULL DEFAULT 'task',
+    is_active boolean NOT NULL DEFAULT true,
+    is_public boolean NOT NULL DEFAULT false,
+    deleted boolean NOT NULL DEFAULT false,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (item_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.items
+    OWNER to postgres;
+
+COMMENT ON TABLE public.items
+    IS 'Base list items (there is more data in the list_items table)';
+
+
+DROP TABLE IF EXISTS public.list_items
+
+CREATE TABLE public.list_items
+(
+    list_items_id uuid NOT NULL DEFAULT gen_random_uuid(),
+    name text,
+    type text NOT NULL DEFAULT 'task',
+    units text,
+    list_id uuid NOT NULL,
+    item_id uuid NOT NULL,
+    quantity real DEFAULT 1,
+    deadline timestamp with time zone,
+    sort_order integer,
+    ongoing integer NOT NULL DEFAULT 0,
+    assignee uuid,
+    completed boolean NOT NULL DEFAULT false,
+    deleted boolean NOT NULL DEFAULT false,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now(),
+    PRIMARY KEY (list_items_id),
+    FOREIGN KEY (list_id)
+        REFERENCES public.lists (list_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID,
+    FOREIGN KEY (item_id)
+        REFERENCES public.items (item_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.list_items
+    OWNER to postgres;
+
+COMMENT ON TABLE public.list_items
+    IS 'Link between the items and the lists';
